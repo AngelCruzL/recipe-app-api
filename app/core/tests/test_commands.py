4701db467d0cd3149e0690c22c04df727b7db0b2
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-11-21 20:17:43 -0600

    feat: add database connection

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -17,7 +17,7 @@ class CommandTests(SimpleTestCase):
         """Test waiting for db when db is available"""
         patched_check.return_value = True
         call_command('wait_for_db')
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
@@ -28,4 +28,4 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patch_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
