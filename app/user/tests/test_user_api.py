330645c84b5b9e0085611fccc21c61bfd1cfe13e
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-02 12:11:00 -0600

    Fix lint errors and change min password length from 5 to 8

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -37,7 +37,9 @@ class PublicUserApiTests(TestCase):
         self.assertNotIn("password", res.data)
 
     def test_user_with_email_exists_error(self):
-        """Test should return an error if a user with the email already exists"""
+        """
+        Test should return an error if a user with the email already exists
+        """
         payload = {
             "email": "success@test.com",
             "password": "testpass123",
@@ -49,7 +51,9 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short_error(self):
-        """Test should return an error if the password has less than 5 characters"""
+        """
+        Test should return an error if the password has less than 8 characters
+        """
         payload = {
             "email": "success@test.com",
             "password": "pass",
