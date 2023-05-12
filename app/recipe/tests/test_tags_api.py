c4a9cdd32719cc236cdc514a3e7bc0e7ea3b013c
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-11 20:26:14 -0600

    Add functionality & test case to update a tag

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -12,6 +12,11 @@ from rest_framework.test import APIClient
 TAGS_URL = reverse('recipe:tag-list')
 
 
+def detail_url(tag_id):
+    """Create and return a tag detail URL"""
+    return reverse('recipe:tag-detail', args=[tag_id])
+
+
 def create_user(email='test@user.com', password='testPass123'):
     """Create and return a sample user"""
     return get_user_model().objects.create_user(email, password)
@@ -66,3 +71,15 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+    def test_update_tag(self):
+        """Test updating a tag"""
+        tag = Tag.objects.create(user=self.user, name='Vegan')
+        payload = {'name': 'New Tag Name'}
+        url = detail_url(tag.id)
+
+        res = self.client.patch(url, payload)
+        tag.refresh_from_db()
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(tag.name, payload['name'])
