3cd846a4e5573e3c78a70e66c7f33628542b4bda
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-13 09:41:48 -0600

    Pass test cases to update tags

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -33,10 +33,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['id']
 
-    def create(self, validated_data):
-        """Create a recipe"""
-        tags = validated_data.pop('tags', [])
-        recipe = Recipe.objects.create(**validated_data)
+    def _get_or_create_tags(self, recipe, tags):
+        """Get or create tags"""
         auth_user = self.context['request'].user
 
         for tag in tags:
@@ -46,7 +44,27 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def create(self, validated_data):
+        """Create a recipe"""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(recipe, tags)
+
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update a recipe"""
+        tags = validated_data.pop('tags', None)
+
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(instance, tags)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
