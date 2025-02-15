11329a8f9d1b3984e2c9c0f3ea5ef5352dfc86e5
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2025-02-15 15:30:19 -0600

    Add environmental variables support

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qm1^a@yk#y-84#h+nwlj66hcs&*eb#tkchzbgzrk90-z+#x#d!'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-#&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 # Application definition
 
