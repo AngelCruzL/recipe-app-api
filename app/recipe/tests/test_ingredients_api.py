b24d23154427dc377fe385ca0862fa41b64bb42f
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2025-02-14 22:36:42 -0600

    Add functionality to delete an ingredient

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -81,3 +81,14 @@ class PrivateIngredientApiTests(TestCase):
 
         ingredient.refresh_from_db()
         self.assertEqual(ingredient.name, payload['name'])
+
+    def test_delete_ingredient(self):
+        """Test deleting an ingredient"""
+        ingredient = Ingredient.objects.create(user=self.user, name='Cabbage')
+        url = detail_url(ingredient.id)
+
+        res = self.client.delete(url)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+
+        ingredients = Ingredient.objects.filter(user=self.user)
+        self.assertFalse(ingredients.exists())
